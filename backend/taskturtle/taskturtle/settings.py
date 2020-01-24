624f9c5e7660ab575d599307b2291660770903e5
Hessam <hessam.hashemi21@gmail.com>
2020-01-25 02:49:51 +0330

    added allowed hosts

--- backend/taskturtle/taskturtle/settings.py
+++ backend/taskturtle/taskturtle/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '00_a0jidsgvo9t6-_1(%m01r0!d#sjtk4rp8mv%z4$bp6^1re!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cloudnative.ir', '137.74.64.232']
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 
 SIMPLE_JWT = {
