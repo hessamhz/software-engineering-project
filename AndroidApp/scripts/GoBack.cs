8beed682302a3ccda322d83d24b987c8456dfc1f
SoroorAghamiri <aghamiri.soroor@gmail.com>
2020-01-28 11:35:47 +0330

    Add files via upload

---
+++ AndroidApp/scripts/GoBack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBack : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+		}
+	}
+}
