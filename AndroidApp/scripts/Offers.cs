a3b6b0eb30068e286d4d216bb2fe789d880deba6
SoroorAghamiri <aghamiri.soroor@gmail.com>
2020-01-28 11:31:50 +0330

    offers added

---
+++ AndroidApp/scripts/Offers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Offers : MonoBehaviour {
+	public GameObject offerPanel;
+	public GameObject paymentPanel;
+
+	public Image offer;
+	public Image payment;
+
+	public Text status;
+	public void onOfferSelected(){
+		status.text = "Payment";
+		payment.GetComponent<Image> ().color = offer.GetComponent<Image> ().color;
+		paymentPanel.SetActive (false);
+		offerPanel.SetActive (true);
+	}
+}
