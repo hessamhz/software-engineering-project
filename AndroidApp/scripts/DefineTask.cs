de13a451a41506a69f0159dd4566d7a5a3b8cb33
SoroorAghamiri <aghamiri.soroor@gmail.com>
2020-01-28 11:17:55 +0330

    TaskDefinition added

---
+++ AndroidApp/scripts/DefineTask.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefineTask : MonoBehaviour {
+	public GameObject newTaskPanel;
+	public List<InputField> inputsFields;
+	public List<Text> result;
+	public Image offer;
+	public Image Definition;
+	public GameObject offerPanel;
+
+	public void onPanelTouched(){
+		newTaskPanel.SetActive (false);
+	}
+
+	public void onSubminClicked(){
+		for (int i = 0; i < inputsFields.Count; i++) {
+			result [i].text = inputsFields [i].text;
+		}
+		result[6].text = "InProgress...";
+		result[7].text = "Waiting for offer...";
+		for (int i = 0; i < inputsFields.Count; i++) {
+			inputsFields[i].text = "";
+		}
+		newTaskPanel.SetActive (true);
+		offer.GetComponent<Image> ().color = Definition.GetComponent<Image> ().color;
+		offerPanel.SetActive (false);
+	}
+
+
+}
